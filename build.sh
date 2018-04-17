3ba7854245f773eb927d3572c9632179608c1079
Oleg Pimenov <pimenov.o@gmail.com>
2018-04-17 13:53:14 +0300

    travis build

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-npm ca clean
-npm install
-
-grunt
--- scripts/install_sketchtool.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-[[ -x sketchtool/bin/sketchtool ]] && exit 0
-
-curl -L -o sketch.zip http://www.sketchapp.com/static/download/sketch.zip
-unzip -qo sketch.zip
-cp -rp Sketch.app/Contents/Resources/sketchtool .
