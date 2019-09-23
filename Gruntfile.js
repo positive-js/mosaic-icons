9fe39adba5a7f8e87ce8a47e0bd015982f41efb7
Margarita Iakovleva <myakovleva@ptsecurity.com>
2019-09-23 17:54:08 +0300

    Revert "feat(docs): path"
    This reverts commit 10630ba3e3919c98845a0596e1dda2c58cae48b8.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,4 @@
-//import {requires} from "grunt";
-
 var FONT_VERSION = '1.9.5';
-
-//var path = requires('path');
 
 var CODEPOINTS = {
     'angle-down-L_16'           : 0xF101,
@@ -62,10 +58,6 @@ const PATH_BUILD_ICONS = './build/icons',
 
 const SKETCH_FILE_DEF = 'mosaic-icons-iconset.sketch';
 
-//const ABSOLUT_PATH = path.resolve();
-
-//console.log(ABSOLUT_PATH);
-
 
 module.exports = function (grunt) {
 
@@ -119,7 +111,7 @@ module.exports = function (grunt) {
         shell: {
             publish: {command: 'npm publish'},
             svgfromsubfolder : {
-                command: 'find ' + PATH_BUILD_ICONS + ' -mindepth 2 -type f -print -exec mv {} ' + PATH_BUILD_ICONS + '/ \\;'
+                command: 'find ' + PATH_BUILD_ICONS + ' -mindepth 2 -type f -print -exec mv {} ' + PATH_BUILD_ICONS + '/ \\;'  
               },
             svgrename: {
                 command: 'cd build/icons && for f in *.svg; do mv "$f" "${f#mc-}"; done'
